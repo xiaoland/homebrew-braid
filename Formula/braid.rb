34aa0d029fa2bced66b423c8baa00ec418cf3aa1
Lan_zhijiang <lanzhijiang@foxmail.com>
2026-08-24 17:12:10 +0800

    Update braid v0.1.0 SHA256 for collapsed migrations

--- Formula/braid.rb
+++ Formula/braid.rb
@@ -2,7 +2,7 @@ class Braid < Formula
   desc "GitHub working memory for local coding agents"
   homepage "https://github.com/xiaoland/braid"
   url "https://github.com/xiaoland/braid/releases/download/v0.1.0/braid-v0.1.0-aarch64-apple-darwin.tar.gz"
-  sha256 "ead51c3525b87b3a2a92865a3b5bf2327f508b350ef4fc0b14c535fb7d567424"
+  sha256 "f50c53fb6636feb35aa737469865748d2375a6aad46535d5f3af12f16b7a0129"
   license "MIT"
 
   depends_on arch: :arm64
