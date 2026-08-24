55669d4542773a0a3b43b5ef76188278cc89cf26
Lan_zhijiang <lanzhijiang@foxmail.com>
2026-08-24 16:31:49 +0800

    Update braid v0.1.0 SHA256 for POST manifest form

--- Formula/braid.rb
+++ Formula/braid.rb
@@ -2,7 +2,7 @@ class Braid < Formula
   desc "GitHub working memory for local coding agents"
   homepage "https://github.com/xiaoland/braid"
   url "https://github.com/xiaoland/braid/releases/download/v0.1.0/braid-v0.1.0-aarch64-apple-darwin.tar.gz"
-  sha256 "506e536b9d08139f4cf6139ae9a9d4fb6f9844e035d0676268789a1e88481e29"
+  sha256 "ead51c3525b87b3a2a92865a3b5bf2327f508b350ef4fc0b14c535fb7d567424"
   license "MIT"
 
   depends_on arch: :arm64
