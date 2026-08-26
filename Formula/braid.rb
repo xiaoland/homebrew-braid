fbade62e33e1f5d9c61e0a04e5da81a47ab554b7
Lan_zhijiang <lanzhijiang@foxmail.com>
2026-08-26 14:37:17 +0800

    Update braid to v0.2.3

--- Formula/braid.rb
+++ Formula/braid.rb
@@ -1,8 +1,8 @@
 class Braid < Formula
   desc "GitHub working memory for local coding agents"
   homepage "https://github.com/xiaoland/braid"
-  url "https://github.com/xiaoland/braid/releases/download/v0.2.2/braid-v0.2.2-aarch64-apple-darwin.tar.gz"
-  sha256 "de5035cc77700020a7bc04c0c14b07dd14192dc281c5af6e328168b6cc881cea"
+  url "https://github.com/xiaoland/braid/releases/download/v0.2.3/braid-v0.2.3-aarch64-apple-darwin.tar.gz"
+  sha256 "c0d6e20cc2868bcda3f4fcaf5b44e1b8283f4378373cc2618e7bc29f548c9987"
   license "MIT"
 
   depends_on arch: :arm64
